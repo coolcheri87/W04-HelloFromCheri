550a10b4f1b08c6c712392fd09b76f6627eae437
coolcheri87 <coolcheri87@gmail.com>
2020-10-10 22:11:28 -0700

    Source to add

--- src/HelloFromCheri.java
+++ src/HelloFromCheri.java
@@ -7,5 +7,6 @@ public class HelloFromCheri {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Cheri");
+        System.out.println("Hello, my name is Cheri2");
     }
 }
