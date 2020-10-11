c684e30d21ceb8d9195e21897e2a87deea68cc0e
coolcheri87 <coolcheri87@gmail.com>
2020-10-10 22:16:43 -0700

    Source to update

--- src/HelloFromCheri.java
+++ src/HelloFromCheri.java
@@ -8,5 +8,6 @@ public class HelloFromCheri {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Cheri");
         System.out.println("Hello, my name is Cheri2");
+        System.out.println("Hello, my name is Cheri3");
     }
 }
