67348c7598d215b52336ea24fcccff77389f3186
coolcheri87 <coolcheri87@gmail.com>
2020-10-10 18:50:27 -0700

    Source to add

---
+++ src/HelloFromCheri.java
@@ -0,0 +1,11 @@
+// HelloFromCheri.java
+// Cheri Hansen - han19067@byui.edu
+// Created 10/10/2020
+// This says hello!
+
+public class HelloFromCheri {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Cheri");
+    }
+}
